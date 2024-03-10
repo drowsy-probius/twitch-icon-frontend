3a43c9ca4a124db8223be6c13506fab1597d78cb
drowsy-probius <k123s456h@gmail.com>
2024-03-10 13:59:50 +0900

    fix fetch promise.all issue

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Search from "./components/Search";
 import SearchResults from './components/SearchList';
 import StatsList from "./components/Stats";
 
-
+type StreamerFetchResult = [string, IconList];
 
 function App() {
   const [showStats, setShowStats] = useState<boolean>(false);
@@ -20,47 +20,60 @@ function App() {
   const SearchInputHandler = (searchKey: string, streamer: string) => {
     setSearchKey(searchKey);
     setSearchStreamer(streamer);
-  }
+  };
 
   const switchButtonHandler = (/* e: React.MouseEvent */) => {
     setShowStats(!showStats);
-  }
+  };
 
   useEffect(() => {
     (async () => {
-      const streamersJson = await (await fetch("./list")).json();
+      const streamersJson: StreamerData[] = await(await fetch("./list")).json();
       const streamers: string[] = [];
       const newData: IconMetatdata = {};
       const newStats: IconWithStats[] = [];
 
-      const tempData = await Promise.all(streamersJson.map(async (streamerInfo: StreamerData) => {
-        const streamerTwitchName = streamerInfo.name.twitch;
-        const streamerIcon: IconList = await (await fetch(`./list/${streamerTwitchName}`)).json();
-        for(let i=0; i<20; i++)
-        {
-          const randidx = Math.floor(Math.random() * streamerIcon.icons.length);
-          const icon = streamerIcon.icons[randidx];
-          newStats.push({
-            ...icon,
-            stats: Math.floor(Math.random() * 20),
-            streamer: streamerTwitchName,
-          });
-        }
+      const fetchResults = await Promise.all(
+        streamersJson.map(
+          async (
+            streamerInfo: StreamerData
+          ): Promise<StreamerFetchResult | null> => {
+            try {
+              const streamerTwitchName = streamerInfo.name.twitch;
+              const streamerIcon: IconList = await (
+                await fetch(`./list/${streamerTwitchName}`)
+              ).json();
+              for (let i = 0; i < 20; i++) {
+                const randidx = Math.floor(
+                  Math.random() * streamerIcon.icons.length
+                );
+                const icon = streamerIcon.icons[randidx];
+                newStats.push({
+                  ...icon,
+                  stats: Math.floor(Math.random() * 20),
+                  streamer: streamerTwitchName,
+                });
+              }
 
-        streamerIcon.icons = streamerIcon.icons.map(i => {
-          i.streamer = streamerTwitchName;
-          return i;
-        });
-      
-        const ret: any[] = [];
-        ret.push(streamerTwitchName);
-        ret.push(streamerIcon);
-        return ret;
-      }));
+              streamerIcon.icons = streamerIcon.icons.map((i) => {
+                i.streamer = streamerTwitchName;
+                return i;
+              });
 
-      tempData.forEach((data: any[]) => {
-        streamers.push(data[0] as string);
-        newData[data[0] as string] = data[1];
+              return [streamerTwitchName, streamerIcon];
+            } catch (e) {
+              console.error(e);
+              return null;
+            }
+          }
+        )
+      );
+
+      fetchResults.forEach((data: StreamerFetchResult | null) => {
+        if (!data) return;
+
+        streamers.push(data[0]);
+        newData[data[0]] = data[1];
       });
 
       setStreamerList(streamers);
@@ -70,20 +83,22 @@ function App() {
   }, []);
 
   return (
-    <div className='App'>
-      <button className={`switch-button ${showStats ? 'search' : 'stats'} `} onClick={switchButtonHandler}>
-      </button>
-      {
-        showStats 
-        ? 
+    <div className="App">
+      <button
+        className={`switch-button ${showStats ? "search" : "stats"} `}
+        onClick={switchButtonHandler}
+      ></button>
+      {showStats ? (
         <StatsList statistics={stats} />
-        :
-        <>
-          <Search streamers={streamerList} inputHandler={SearchInputHandler}>
-            <SearchResults data={data} searchKey={searchKey} streamer={searchStreamer} />
-          </Search>      
-        </>
-      }
+      ) : (
+        <Search streamers={streamerList} inputHandler={SearchInputHandler}>
+          <SearchResults
+            data={data}
+            searchKey={searchKey}
+            streamer={searchStreamer}
+          />
+        </Search>
+      )}
     </div>
   );
 }
